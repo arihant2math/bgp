130ad845da6960971d4ce68b8623a7155af14c22
Ashwin Naren <arihant2math@gmail.com>
2026-05-02 15:54:13 -0700

    fix a little

--- src/pages/Repository.tsx
+++ src/pages/Repository.tsx
@@ -1,4 +1,7 @@
 import RepoNavbar from "../components/RepoNavbar";
+import { useQuery } from "@tanstack/solid-query";
+import { Match, Switch } from "solid-js";
+import { getOctokit, parseRestOctokitResponse } from "../lib/octokit.ts";
 
 export type RepositoryProps = {
     profile: string;
@@ -6,6 +9,14 @@ export type RepositoryProps = {
 };
 
 function Repository(props: RepositoryProps) {
+    const metadataQuery = useQuery(() => ({
+        queryKey: ["repoMetadata", props.profile, props.repo],
+        queryFn: () =>
+            getOctokit()
+                .rest.repos.get({ owner: props.profile, repo: props.repo })
+                .then((res) => parseRestOctokitResponse(res)),
+    }));
+
     return (
         <main>
             <RepoNavbar
@@ -13,6 +24,25 @@ function Repository(props: RepositoryProps) {
                 repo={props.repo}
                 active="code"
             />
+            <Switch>
+                <Match when={metadataQuery.isPending}>Loading...</Match>
+                <Match when={metadataQuery.isError}>Error</Match>
+                <Match when={metadataQuery.isSuccess}>
+                    <div class="flex w-full flex-col mx-8">
+                        <div class="flex flex-row">
+                            <h1 class="text-xl">{metadataQuery.data.name}</h1>
+                            <div class="badge badge-ghost text-xs">{metadataQuery.data.visibility}</div>
+                            <div class="flex flex-1 items-end">
+                                <button class="btn btn-xs">Watch</button>
+                                <button class="btn btn-xs">Fork</button>
+                                <button class="btn btn-xs">Star</button>
+                            </div>
+                        </div>
+                        <div class="divider"></div>
+                        <p>{metadataQuery.data.description}</p>
+                    </div>
+                </Match>
+            </Switch>
         </main>
     );
 }
