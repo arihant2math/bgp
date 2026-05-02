064fecc1ff18f62816d3719ee5ec496be985c5ac
Ashwin Naren <arihant2math@gmail.com>
2026-05-02 16:37:29 -0700

    add contents

--- src/pages/Repository.tsx
+++ src/pages/Repository.tsx
@@ -1,8 +1,9 @@
 import RepoNavbar from "../components/RepoNavbar";
 import { useQuery } from "@tanstack/solid-query";
-import { Match, Switch } from "solid-js";
+import {For, Match, Switch} from "solid-js";
 import { getOctokit, parseRestOctokitResponse } from "../lib/octokit.ts";
 import { approximateNumber as approx } from 'approximate-number';
+import Octicon from "../components/Octicon.tsx";
 
 export type RepositoryProps = {
     profile: string;
@@ -18,6 +19,21 @@ function Repository(props: RepositoryProps) {
                 .then((res) => parseRestOctokitResponse(res)),
     }));
 
+    let contentsQuery = useQuery(() => ({
+        queryKey: ["contents", props.profile, props.repo],
+        queryFn: () =>
+
+            getOctokit()
+                .rest.repos.getContent({ owner: props.profile, repo: props.repo, path: "" })
+                .then((res) => {
+                    let data = parseRestOctokitResponse(res);
+                    data.sort((a, b) => {
+                        a.type.localeCompare(b.type)
+                    });
+                    return data;
+                }),
+    }));
+
     return (
         <main>
             <RepoNavbar
@@ -26,7 +42,7 @@ function Repository(props: RepositoryProps) {
                 active="code"
             />
             <Switch>
-                <Match when={metadataQuery.isPending}>Loading...</Match>
+                <Match when={metadataQuery.isPending}>Loading ...</Match>
                 <Match when={metadataQuery.isError}>Error</Match>
                 <Match when={metadataQuery.isSuccess}>
                     <div class="flex flex-col mx-8">
@@ -43,6 +59,31 @@ function Repository(props: RepositoryProps) {
                         <div class="flex flex-row">
                             <div class="flex-3">
                                 {metadataQuery.data.default_branch}
+                                <Switch>
+                                    <Match when={contentsQuery.isPending}>Loading ...</Match>
+                                    <Match when={contentsQuery.isError}>Error</Match>
+                                    <Match when={contentsQuery.isSuccess}>
+                                        <ul class="list">
+                                            <For each={contentsQuery.data}>
+                                                {(item, index) =>
+                                                    <li class="list-row">
+                                                        <a href="#" class="flex items-center gap-2">
+                                                            <Switch>
+                                                                <Match when={item.type === "dir"}>
+                                                                    <Octicon name="file-directory" size={16} aria-hidden="true" />
+                                                                </Match>
+                                                                <Match when={item.type === "file"}>
+                                                                    <Octicon name="file" size={16} aria-hidden="true" />
+                                                                </Match>
+                                                            </Switch>
+                                                            {item.name}
+                                                        </a>
+                                                    </li>
+                                                }
+                                            </For>
+                                        </ul>
+                                    </Match>
+                                </Switch>
                             </div>
                             <div class="flex-1">
                                 <b>About</b>
