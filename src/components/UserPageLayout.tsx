998401e9a2aab10791cae94228914003bb05d99a
Ashwin Naren <arihant2math@gmail.com>
2026-05-02 21:33:15 -0700

    more profile data

--- src/components/UserPageLayout.tsx
+++ src/components/UserPageLayout.tsx
@@ -1,7 +1,8 @@
-import type { JSX } from "solid-js";
+import {type JSX, Show} from "solid-js";
 import Avatar from "./Avatar.tsx";
 import UserNavbar from "./UserNavbar.tsx";
 import type { UserTab } from "./UserNavbar.tsx";
+import Octicon from "./Octicon.tsx";
 
 export type UserPageLayoutProps = {
     profile: string;
@@ -16,13 +17,22 @@ function UserPageLayout(props: UserPageLayoutProps) {
             <UserNavbar profile={props.profile} active={props.active} />
             <div class="flex gap-8 max-w-6xl mx-auto mt-8">
                 <aside class="w-72 shrink-0">
-                    <Avatar
-                        href={props.profileData.data.avatar_url}
-                        size={296}
-                        alt={`${props.profile}'s avatar`}
-                        class="border border-base-300 bg-base-200"
-                    />
-                    <h1 class="text-2xl">{props.profileData.data.name}</h1>
+                    <div class="flex flex-col gap-2">
+                        <Avatar
+                            href={props.profileData.data.avatar_url}
+                            size={296}
+                            alt={`${props.profile}'s avatar`}
+                            class="border border-base-300 bg-base-200"
+                        />
+                        <h1 class="text-2xl">{props.profileData.data.name}</h1>
+                        <p class="text-lg">{props.profileData.data.login}</p>
+                        <button class="btn w-full">Follow</button>
+                        <p>{props.profileData.data.bio}</p>
+                        <Show when={props.profileData.data.location !== null}>
+                            <div class="flex items-center flex-row gap-2"><Octicon name="location" size={16} aria-hidden="true" /> {props.profileData.data.location}</div>
+                        </Show>
+                        {/*<p>{JSON.stringify(props.profileData.data)}</p>*/}
+                    </div>
                 </aside>
                 <div class="flex-1">{props.children}</div>
             </div>
